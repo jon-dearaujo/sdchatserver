9ae522b7e98f746e1b5c3f6f452719c2473fbd97
Jonathan <jonathanaraujo26@github.com>
2014-03-01 20:48:39 -0300

    Dev. de captura para nome do cliente no momento da conexão com o server.

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,6 +1,8 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -36,10 +38,21 @@ public class Server
 	{
 		while (isAlive())
 		{
-			Socket connectedClientsocket = getNewConnectedClientsocket();
-			
+			try
+			{
+				Socket connectedClientsocket = getNewConnectedClientsocket();
+				BufferedReader clientIn = 
+						new BufferedReader( 
+								new InputStreamReader(connectedClientsocket.getInputStream()));
+				String clientName = clientIn.readLine();
+				ConnectedClientManager.getInstance()
+					.addAndStart(new ConnectedClient(clientName, connectedClientsocket));
+			} catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
 	}
 
 	private Socket getNewConnectedClientsocket()
