82e691e5bbd587a6ade87f1322c9b1f81915c08e
Jonathan <jonathanaraujo26@github.com>
2014-03-02 20:46:04 -0300

    GUI

--- src/gui/ServerFrame.java
+++ src/gui/ServerFrame.java
@@ -1,23 +1,61 @@
 package gui;
 
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 
 public class ServerFrame extends JFrame
 {
 	public ServerFrame()
 	{
+		configureLayout();
+		basicConfiguration();
+		this.setVisible(true);
+	}
+
+	private void basicConfiguration()
+	{
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setTitle("Server Chat");
-		this.setSize(300, 300);
+		this.setSize(500, 300);
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
-		this.setVisible(true);
-		Logger log = Logger.getLogger(this.getClass().getName());
-		log.warning("START!");
+	}
+	
+	private void configureLayout()
+	{
+		
+		GridBagLayout layout = new GridBagLayout();
+		GridBagConstraints constraints = new GridBagConstraints();
+		Container container = this.getContentPane();
+		container.setLayout(layout);
+		
+		JButton startButton = new JButton(new ImageIcon("res/start.png"));
+		startButton.setToolTipText("Start server");
+		JButton stopButton = new JButton(new ImageIcon("res/stop.png"));
+		JTextArea logArea = new JTextArea();
+		stopButton.setToolTipText("Stop server");
+		JScrollPane logScrollPane = new JScrollPane(logArea);
+		
+		constraints.weightx = 1;
+		constraints.weighty = 0.1;
+		constraints.ipady = -7;
+		constraints.ipadx = -30;
+		container.add(startButton, constraints);
+		container.add(stopButton, constraints);
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.gridy = 1;
+		constraints.ipadx = 0;
+		constraints.ipady = 0;
+		constraints.gridwidth = 2;
+		constraints.weighty = 1;
+		container.add(logScrollPane, constraints);
 	}
 }
